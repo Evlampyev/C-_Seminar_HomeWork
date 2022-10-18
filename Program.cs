5d0324d5964bd790943ae6aace100f79a09a35aa
Bad_S <Evlampyev.Alex@gmail.com>
2022-10-18 23:10:02 +0500

    Second Seminar homework Evlampyev

--- Program.cs
+++ Program.cs
@@ -54,5 +54,55 @@ while (current <= N)
     Console.Write(current + " ");
     current += 2;
 }
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Задача №10
 
-*/
+int SecondDig(int num)
+{
+    int result = num / 10 % 10;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+int SecondDigit = SecondDig(number);
+Console.WriteLine ($"Second digit of {number} is {SecondDigit}");
+
+Задача №13
+
+int ThirdDig(int num)
+{
+    int tri = -1;
+    if (num > 99)
+    {
+        while (num > 999)
+            num = num / 10;
+        tri = num % 10;
+    }    
+    return tri;
+}
+
+Console.Write("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = ThirdDig(number);
+if (result == -1)
+    Console.WriteLine("There is no such figure");
+else
+    Console.WriteLine($"The third figure is {result}");
+
+
+    Задача №15
+
+string DayWeek (int Day)
+    {
+        if (Day == 6 || Day ==7)
+            return "Yes";
+        else
+            return "No";
+    }
+
+
+Console.Write("Input Day of week ");
+int den = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DayWeek(den));
+ */
+ 
